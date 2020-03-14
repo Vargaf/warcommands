1e7f1a88647c7ce67c2e505182f2e44f3620f3f2
Vargaf <fernandovillarp@gmialcom>
2020-03-14 13:31:33 +0100

    command data on command components unsubscriptions

--- warcommands-app/src/app/commands-panel/commands/if-then-else/if-then-else.component.ts
+++ warcommands-app/src/app/commands-panel/commands/if-then-else/if-then-else.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { IfThenElseCommandEntity } from 'src/warcommands/commands-panel/domain/command/model/if-then-else-command.entity';
 import { CommandNgrxRepositoryService } from 'src/warcommands/commands-panel/infrastructure/ngrx/command/command-ngrx-repository.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-if-then-else',
     templateUrl: './if-then-else.component.html',
     styleUrls: ['./if-then-else.component.scss']
 })
-export class IfThenElseComponent implements OnInit {
+export class IfThenElseComponent implements OnInit, OnDestroy {
 
     @Input() commandData: IfThenElseCommandEntity;
 
     thenCommandContainerId: string;
     elseCommandContainerId: string;
+
+    private commandDataSubscription: Subscription;
 
     constructor(
         private readonly commandNgrxRepositoryService: CommandNgrxRepositoryService
@@ -22,10 +25,14 @@ export class IfThenElseComponent implements OnInit {
         if (this.commandData) {
             this.thenCommandContainerId = this.commandData.innerCommandContainerIdList.thenCommandContainerId;
             this.elseCommandContainerId = this.commandData.innerCommandContainerIdList.elseCommandContainerId;
-            this.commandNgrxRepositoryService.getCommand(this.commandData.id).subscribe((command) => {
+            this.commandDataSubscription = this.commandNgrxRepositoryService.getCommand(this.commandData.id).subscribe((command) => {
                 this.commandData = (command as IfThenElseCommandEntity);
             });
         }
+    }
+
+    ngOnDestroy() {
+        this.commandDataSubscription.unsubscribe();
     }
 
 }
--- warcommands-app/src/app/commands-panel/commands/if-then/if-then.component.ts
+++ warcommands-app/src/app/commands-panel/commands/if-then/if-then.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { IfThenCommandEntity } from 'src/warcommands/commands-panel/domain/command/model/if-then-command.entity';
 import { CommandNgrxRepositoryService } from 'src/warcommands/commands-panel/infrastructure/ngrx/command/command-ngrx-repository.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-if-then',
     templateUrl: './if-then.component.html',
     styleUrls: ['./if-then.component.scss']
 })
-export class IfThenComponent implements OnInit {
+export class IfThenComponent implements OnInit, OnDestroy {
 
     @Input() commandData: IfThenCommandEntity;
 
     thenCommandContainerId: string = undefined;
+
+    private commandDataSubscription: Subscription;
 
     constructor(
         private readonly commandNgrxRepositoryService: CommandNgrxRepositoryService
@@ -20,10 +23,14 @@ export class IfThenComponent implements OnInit {
     ngOnInit() {
         if (this.commandData) {
             this.thenCommandContainerId = this.commandData.innerCommandContainerIdList.thenCommandContainerId;
-            this.commandNgrxRepositoryService.getCommand(this.commandData.id).subscribe((command) => {
+            this.commandDataSubscription = this.commandNgrxRepositoryService.getCommand(this.commandData.id).subscribe((command) => {
                 this.commandData = (command as IfThenCommandEntity);
             });
         }
+    }
+
+    ngOnDestroy() {
+        this.commandDataSubscription.unsubscribe();
     }
 
 }
