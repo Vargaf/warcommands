cb6df9c4a958a7b5a2e32ca18f873ae02885acd7
fernando.villar <fernando.villar@europcar.com>
2023-01-10 18:58:22 +0100

    Working on the first tutorial

--- warcommands-app/src/app/tutorial/first-worker/first-worker.component.ts
+++ warcommands-app/src/app/tutorial/first-worker/first-worker.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatStepper} from "@angular/material/stepper";
 
 @Component({
-  selector: 'app-first-worker',
-  templateUrl: './first-worker.component.html',
-  styleUrls: ['./first-worker.component.scss']
+    selector: 'app-first-worker',
+    templateUrl: './first-worker.component.html',
+    styleUrls: ['./first-worker.component.scss']
 })
 export class FirstWorkerComponent {
 
-  constructor() { }
+    @ViewChild('stepper', { static: false })
+    stepper!: MatStepper;
+
+    selectedIndex = 0;
+
+    constructor() {
+    }
+
+    next(): void {
+        if(this.stepper.steps.length > this.selectedIndex) {
+            this.selectedIndex++;
+        }
+        console.log(this.selectedIndex);
+    }
+
+    back():void {
+        if(this.selectedIndex > 0) {
+            this.selectedIndex--;
+        }
+        console.log(this.selectedIndex);
+    }
 
 }
